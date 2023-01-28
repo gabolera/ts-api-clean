4cae2eef9809ac9db5942af756da726a5cb2b486
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2023-01-28 00:34:27 -0300

    chore: create a Http protocol

---
+++ src/presentation/protocols/HttpProtocol.ts
@@ -0,0 +1,31 @@
+class Http {
+  private statusCode: number = 200
+  private data: any
+
+  status(code: number) {
+    this.statusCode = code
+    return this
+  }
+
+  json(data: any = ''): Response {
+    this.data = data
+
+    return {
+      statusCode: this.statusCode,
+      data: this.data ?? '',
+    }
+  }
+}
+
+export default new Http()
+
+export interface Request {
+  headers?: any
+  params?: any
+  body?: any
+}
+
+export interface Response {
+  statusCode: any
+  data?: any
+}
