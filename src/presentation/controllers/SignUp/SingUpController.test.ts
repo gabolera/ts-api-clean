be16ff8926d54c7db68056b70403dc46227d926f
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2023-01-28 09:47:30 -0300

    test: :ambulance: create a email validation test for signupcontroller

--- src/presentation/controllers/SignUp/SingUpController.test.ts
+++ src/presentation/controllers/SignUp/SingUpController.test.ts
@@ -1,5 +1,29 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vitest } from 'vitest'
 import { SignUpController } from './SignUpController'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { EmailValidator } from './protocols/EmailValidator'
+
+interface SutTypes {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    public isValid = true
+    verify(email: string): boolean {
+      return this.isValid
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  // const signUpRepository = new signUpRepository()
+  const sut = new SignUpController({ emailValidator: emailValidatorStub })
+
+  return {
+    sut,
+    emailValidatorStub,
+  }
+}
 
 describe('SignUp Controller Test ', () => {
   test('should be 400 if email is not provided', () => {
@@ -10,9 +34,10 @@ describe('SignUp Controller Test ', () => {
         confirm_password: 'any_confirm_password',
       },
     }
-    const sut = new SignUpController().handle(request)
-    expect(sut.statusCode).toEqual(400)
-    expect(sut.data).toEqual(new Error('The email field is missing!'))
+    const { sut } = makeSut()
+    const response = sut.handle(request)
+    expect(response.statusCode).toEqual(400)
+    expect(response.data).toEqual(new MissingParamError('email'))
   })
 
   test('should be 400 if name is not provided', () => {
@@ -23,9 +48,10 @@ describe('SignUp Controller Test ', () => {
         confirm_password: 'any_confirm_password',
       },
     }
-    const sut = new SignUpController().handle(request)
-    expect(sut.statusCode).toEqual(400)
-    expect(sut.data).toEqual(new Error('The name field is missing!'))
+    const { sut } = makeSut()
+    const response = sut.handle(request)
+    expect(response.statusCode).toEqual(400)
+    expect(response.data).toEqual(new MissingParamError('name'))
   })
 
   test('should be 400 if password is not provided', () => {
@@ -36,9 +62,10 @@ describe('SignUp Controller Test ', () => {
         confirm_password: 'any_confirm_password',
       },
     }
-    const sut = new SignUpController().handle(request)
-    expect(sut.statusCode).toEqual(400)
-    expect(sut.data).toEqual(new Error('The password field is missing!'))
+    const { sut } = makeSut()
+    const response = sut.handle(request)
+    expect(response.statusCode).toEqual(400)
+    expect(response.data).toEqual(new MissingParamError('password'))
   })
 
   test('should be 400 if confirm_password is not provided', () => {
@@ -49,24 +76,25 @@ describe('SignUp Controller Test ', () => {
         password: 'any_password',
       },
     }
-    const sut = new SignUpController().handle(request)
-    expect(sut.statusCode).toEqual(400)
-    expect(sut.data).toEqual(
-      new Error('The confirm_password field is missing!')
-    )
+    const { sut } = makeSut()
+    const response = sut.handle(request)
+    expect(response.statusCode).toEqual(400)
+    expect(response.data).toEqual(new MissingParamError('confirm_password'))
   })
 
-  test('should be 200 with make a signup', () => {
+  test('should be 400 if email is invalid param', () => {
     const request = {
       body: {
-        email: 'any_email@mail.com',
+        email: 'invalid_email#mail.com',
         name: 'any_name',
         password: 'any_password',
         confirm_password: 'any_password',
       },
     }
-    const sut = new SignUpController().handle(request)
-    expect(sut.statusCode).toEqual(200)
-    // expect(sut.data).toEqual()
+    const { sut, emailValidatorStub } = makeSut()
+    vitest.spyOn(emailValidatorStub, 'verify').mockReturnValueOnce(false)
+    const response = sut.handle(request)
+    expect(response.statusCode).toEqual(400)
+    expect(response.data).toEqual(new InvalidParamError('email'))
   })
 })
