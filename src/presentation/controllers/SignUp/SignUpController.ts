aa304478c28e3a013937593728b15f2ba52e7a59
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2023-01-28 00:56:36 -0300

    chore: create validations in requests

--- src/presentation/controllers/SignUp/SignUpController.ts
+++ src/presentation/controllers/SignUp/SignUpController.ts
@@ -1,1 +1,16 @@
-export class SignUpController {}
+import { MissingParamError } from '../../protocols/errors/MissingParamError'
+import HttpResponse, { Request, Response } from '../../protocols/HttpProtocol'
+
+export class SignUpController {
+  handle(request: Request): Response {
+    const requiredFields = ['email', 'name', 'password', 'confirm_password']
+
+    for (const validField of requiredFields) {
+      if (!request.body[validField]) {
+        return HttpResponse.status(400).json(new MissingParamError(validField))
+      }
+    }
+
+    return HttpResponse.status(200).json()
+  }
+}
